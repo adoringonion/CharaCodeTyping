0f86e0b556f10a81cf349fd51baac7e517cfb639
adoringonion <adringonion.byazura@gmail.com>
2022-03-10 01:23:14 +0900

    tekiou

---
+++ Assets/CharaCodeTyping/UI/Fonts/MenuButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace CharaCodeTyping.UI.Fonts
+{
+    public class MenuButton : Button
+    {
+        private Image _backGroundImage;
+
+        protected override void Start()
+        {
+            base.Start();
+            _backGroundImage = GetComponent<Image>();
+        }
+
+        public override void OnSelect(BaseEventData eventData)
+        {
+            Debug.Log("aaaa");
+            _backGroundImage.fillAmount += 1f;
+        }
+    }
+}
