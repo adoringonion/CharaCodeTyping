ba292230dd9f6ea1af3886a8c3918ad14ad95bdb
adoringonion <adringonion.byazura@gmail.com>
2022-03-12 21:37:27 +0900

    メニューボタン

--- Assets/CharaCodeTyping/UI/Fonts/MenuButton.cs
+++ Assets/CharaCodeTyping/UI/Fonts/MenuButton.cs
@@ -1,23 +1,25 @@
 ﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace CharaCodeTyping.UI.Fonts
 {
+    [RequireComponent(typeof(Image))]
     public class MenuButton : Button
     {
+        [SerializeField] private float tweenDuration = 0.2f;
         private Image _backGroundImage;
         private Tween _tween;
 
-        protected override void Start()
+        protected override void Reset()
         {
-            base.Start();
             _backGroundImage = GetComponent<Image>();
         }
 
         public override void OnSelect(BaseEventData eventData)
         {
-            _tween = _backGroundImage.DOFillAmount(1, 0.2f);
+            _tween = _backGroundImage.DOFillAmount(1, tweenDuration);
         }
 
         public override void OnDeselect(BaseEventData eventData)
